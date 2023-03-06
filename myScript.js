56c2f1de1a45295a8c946b49988c5789726ab0ee
Matko <tkoma999@gmail.com>
2023-03-06 17:34:57 +0100

    Add playRound function and Game()

---
+++ myScript.js
@@ -0,0 +1,31 @@
+const options=["rock","paper","scissors"];
+let player_points=0;
+let computer_points=0;
+function getComputerChoice(){
+
+  let choice = options[Math.floor(Math.random()*options.length)];
+  return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+
+    if(playerSelection=="rock"&&computerSelection=="scissors"||
+      playerSelection=="scissors"&&computerSelection=="paper"||
+      playerSelection=="paper"&&computerSelection=="rock")return player_points++,console.log("It's a tie!!");
+    else if(playerSelection==computerSelection) return console.log("It's a tie!!")
+    else return computer_points++,console.log("It's a tie!!");
+
+  }
+function game(){
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection=prompt("Input rock,paper or scissors.").toLowerCase();
+        let computerSelection=getComputerChoice();
+        playRound(playerSelection, computerSelection);
+     }
+
+  }
+game();
+   
+ 
